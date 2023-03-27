4c666088f1ab8426088561653f38bd59cbac5654
hongdakyung <bongbk72@naver.com>
2023-03-27 23:33:12 +0900

    Fix : webpack 주석처리
    by@ddoqi

--- next.config.js
+++ next.config.js
@@ -12,9 +12,9 @@ module.exports = async () => {
         },
       ];
     },
-    webpack(config) {
-      config.resolve.alias["@"] = path.resolve(__dirname);
-      return config;
-    },
+    // webpack(config) {
+    //   config.resolve.alias["@"] = path.resolve(__dirname);
+    //   return config;
+    // },
   };
 };
